89fb81e6d8c4d1567897504ddf5cb44fd35eb244
Timur Izmailov <izmailov@mirapolis.ru>
2012-03-20 10:53:51 +0400

    testing with git config --global core.editor

--- testdrive/index.php
+++ testdrive/index.php
@@ -8,7 +8,7 @@ $config=dirname(__FILE__).'/protected/config/main.php';
 defined('YII_DEBUG') or define('YII_DEBUG',true);
 // specify how many levels of call stack should be shown in each log message
 defined('YII_TRACE_LEVEL') or define('YII_TRACE_LEVEL',3);
-
+//gitplay
 require_once($yii);
 Yii::createWebApplication($config)->run();
 $app = Yii::app();
